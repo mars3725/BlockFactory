f5e4b244c80f8dedd2280229d1dd23a5ef4ac211
Matthew Mohandiss <mtm3725@gmail.com>
2019-07-02 10:08:41 -0400

    Initial Commit

---
+++ desktop/src/com/mattmohandiss/blockfactory/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mattmohandiss.blockfactory.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mattmohandiss.blockfactory.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
